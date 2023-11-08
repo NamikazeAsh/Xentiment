393f5a036c0492e42018ea9cdda44477e89b8e3d
NamikazeAsh <ashwinsatish0@gmail.com>
2023-11-08 22:10:10 +0530

    ui done for xentiment, made sm changes to the ui code

--- XentimentApp/urls.py
+++ XentimentApp/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from XentimentApp import views
 
 urlpatterns = [
+    path('',views.XentimentHome,name='home'),
 ]
 
 if settings.DEBUG:
